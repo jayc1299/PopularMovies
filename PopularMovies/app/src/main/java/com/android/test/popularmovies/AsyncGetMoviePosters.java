2aa7214dec291a7001920aed92ea63bc1d28d184
Jay Cunnington <jay.cunnington@gmail.com>
2015-10-01 18:09:40 +0100

    Initial Commit

---
+++ PopularMovies/app/src/main/java/com/android/test/popularmovies/AsyncGetMoviePosters.java
@@ -0,0 +1,14 @@
+package com.android.test.popularmovies;
+
+import android.os.AsyncTask;
+
+public class AsyncGetMoviePosters extends AsyncTask{
+
+	@Override
+	protected Object doInBackground(Object[] params) {
+
+		//http://api.themoviedb.org/3/discover/movie?api_key=XXX&sort_by=popularity.desc
+
+		return null;
+	}
+}
